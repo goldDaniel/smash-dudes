89e35121dd3cd254b29b7164b45c0a1d2eac5f7e
Daniel Gold <gold.daniel@outlook.com>
2021-10-21 19:13:13 -0500

    Inital Commit

---
+++ core/src/main/java/smashdudes/core/Entry.java
@@ -0,0 +1,7 @@
+package smashdudes.core;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Entry extends ApplicationAdapter {
+}
