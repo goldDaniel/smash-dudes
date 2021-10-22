ab6c6ed8a4df093c1a281071666cf2fee776f2f3
Vessalia <nathanielples@outlook.com>
2021-10-21 19:25:24 -0500

    Entry implements ApplicationListener

--- core/src/main/java/smashdudes/core/Entry.java
+++ core/src/main/java/smashdudes/core/Entry.java
@@ -1,7 +1,43 @@
 package smashdudes.core;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Entry extends ApplicationAdapter {
+public class Entry implements ApplicationListener
+{
+    @Override
+    public void create()
+    {
+
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+
+    }
+
+    @Override
+    public void render()
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void resume()
+    {
+
+    }
+
+    @Override
+    public void dispose()
+    {
+
+    }
 }
