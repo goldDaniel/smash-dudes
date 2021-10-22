17d252114cb7913898e8d4db1fa5f7f9d5c4b4f9
Daniel Gold <gold.daniel@outlook.com>
2021-10-21 21:32:32 -0500

    Moving player and camera

--- core/src/main/java/smashdudes/core/Entry.java
+++ core/src/main/java/smashdudes/core/Entry.java
@@ -1,26 +1,82 @@
 package smashdudes.core;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Entry implements ApplicationListener
 {
+    public static float WORLD_WIDTH = 20;
+    public static float WORLD_HEIGHT = 12;
+
+    Viewport viewport;
+    OrthographicCamera camera;
+    ShapeRenderer sh;
+
+    Player player = new Player();
+
     @Override
     public void create()
     {
-
+        sh = new ShapeRenderer();
+        camera = new OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT);
+        viewport = new ExtendViewport(camera.viewportWidth, camera.viewportHeight, camera);
     }
 
     @Override
     public void resize(int width, int height)
     {
-
+        viewport.update(width, height);
+        viewport.apply();
     }
 
     @Override
     public void render()
     {
+        float dt = Gdx.graphics.getDeltaTime();
 
+        player.update(dt);
+
+        float cameraSpeed = 4f;
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+        {
+            camera.translate(-dt * cameraSpeed, 0);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+        {
+            camera.translate(dt * cameraSpeed, 0);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP))
+        {
+            camera.translate(0, dt * cameraSpeed);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+        {
+            camera.translate(0, -dt * cameraSpeed);
+        }
+        camera.update();
+
+
+        ScreenUtils.clear(Color.BLACK);
+
+        sh.setProjectionMatrix(camera.combined);
+        sh.begin(ShapeRenderer.ShapeType.Filled);
+
+        player.draw(sh);
+
+        sh.setColor(Color.GREEN);
+        sh.rect(-8, 1, 3, 0.5f);
+        sh.rect( 5, 1, 3, 0.5f);
+        sh.rect(-10, -5, 20, 0.5f);
+        
+        sh.end();
     }
 
     @Override
---
+++ core/src/main/java/smashdudes/core/Player.java
@@ -0,0 +1,44 @@
+package smashdudes.core;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+
+
+public class Player
+{
+    Vector2 position = new Vector2();
+
+    float speed = 4;
+
+    void update(float dt)
+    {
+        Vector2 velocity = new Vector2();
+        if(Gdx.input.isKeyPressed(Input.Keys.A))
+        {
+            velocity.x--;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.D))
+        {
+            velocity.x++;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.W))
+        {
+            velocity.y++;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.S))
+        {
+            velocity.y--;
+        }
+        velocity.nor().scl(speed * dt);
+        position.add(velocity);
+    }
+
+    void draw(ShapeRenderer sh)
+    {
+        sh.setColor(Color.RED);
+        sh.rect(position.x, position.y, 2, 2);
+    }
+}
