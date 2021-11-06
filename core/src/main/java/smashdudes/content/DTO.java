0ade0580e7195d9fc2c016d6f19e436b6fdf07da
Vessalia <nathanielples@outlook.com>
2021-11-06 17:09:46 -0500

    Character animation data reading

--- core/src/main/java/smashdudes/content/DTO.java
+++ core/src/main/java/smashdudes/content/DTO.java
@@ -33,26 +33,74 @@ public class DTO
 
         public float jumpStrength;
         public float gravity;
+        public float weight;
 
         protected void setPlayerData(JsonValue characterData)
         {
             jumpStrength = characterData.get("jump_strength").asFloat();
             gravity = characterData.get("gravity").asFloat();
+            weight = characterData.get("weight").asFloat();
+
+            terrainCollider.x = characterData.get("terrain_collider").asFloatArray()[0];
+            terrainCollider.y = characterData.get("terrain_collider").asFloatArray()[1];
+
+            JsonValue animationData = characterData.get("animations");
+            for (int i = 0; i < animationData.size; i++)
+            {
+                Animation animation = new Animation();
+                animation.setAnimation(animationData.get(i));
+                animations.add(animation);
+            }
         }
     }
 
-    public static class Animation
+    private static class Animation
     {
         public String textureFilePath;
 
         public Array<AnimationFrame> frames = new Array<>();
+
+        protected void setAnimation(JsonValue animationData)
+        {
+            textureFilePath = animationData.get("texture_path").asString();
+
+            JsonValue animationFrameData = animationData.get("frames");
+            for (int i = 0; i < animationFrameData.size; i++)
+            {
+                AnimationFrame frame = new AnimationFrame();
+                frame.setAnimationFrame(animationFrameData.get(i));
+                frames.add(frame);
+            }
+        }
     }
 
-    public static class AnimationFrame
+    private static class AnimationFrame
     {
         public Rectangle textureRegion;
 
         public Array<Rectangle> hitboxes = new Array<>();
         public Array<Rectangle> hurtboxes = new Array<>();
+
+        protected void setAnimationFrame(JsonValue animationFrameData)
+        {
+            float[] textureData = animationFrameData.get("texture_region").asFloatArray();
+            textureRegion = new Rectangle(textureData[0], textureData[1], textureData[2], textureData[3]);
+
+            JsonValue hitboxData = animationFrameData.get("hitboxes");
+            for (int i = 0; i < hitboxData.size; i++)
+            {
+                float[] hitboxValues = hitboxData.get(i).asFloatArray();
+                Rectangle hitbox = new Rectangle(hitboxValues[0], hitboxValues[1], hitboxValues[2], hitboxValues[3]);
+                hitboxes.add(hitbox);
+            }
+
+            JsonValue hurtboxData = animationFrameData.get("hurtboxes");
+            for (int i = 0; i < hurtboxData.size; i++)
+            {
+                float[] hurtboxValues = hurtboxData.get(i).asFloatArray();
+                Rectangle hurtbox = new Rectangle(hurtboxValues[0], hurtboxValues[1], hurtboxValues[2], hurtboxValues[3]);
+                hurtboxes.add(hurtbox);
+            }
+        }
     }
 }
