914d87e336ee171ab83c1338ec1c77739def16bc
Vessalia <nathanielples@outlook.com>
2021-11-12 09:41:50 -0600

    redundant data storage

--- core/src/main/java/smashdudes/ecs/components/AnimationDebugComponent.java
+++ core/src/main/java/smashdudes/ecs/components/AnimationDebugComponent.java
@@ -10,19 +10,12 @@ public class AnimationDebugComponent extends Component
 {
     public float currentTime;
 
-    public Array<Array<Rectangle>> hitboxes;
-    public Array<Array<Rectangle>> hurtboxes;
-
     public Animation<Array<Rectangle>> currentHitboxes;
     public Animation<Array<Rectangle>> currentHurtboxes;
 
     public AnimationDebugComponent(Array<Array<Rectangle>> hitboxes,  Array<Array<Rectangle>> hurtboxes)
     {
-        this.hitboxes = hitboxes;
-        this.hurtboxes = hurtboxes;
-
         currentHitboxes = new Animation<>(1f / 8f, hitboxes, Animation.PlayMode.LOOP);
-
         currentHurtboxes = new Animation<>(1f / 8f, hurtboxes, Animation.PlayMode.LOOP);
     }
 }
