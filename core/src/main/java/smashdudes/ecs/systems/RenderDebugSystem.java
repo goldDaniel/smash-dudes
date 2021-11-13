43a3c46cd2e2ebc2d073106c5ddb33ab20d3a254
Daniel Gold <gold.daniel@outlook.com>
2021-11-12 19:47:40 -0600

    negated hitbox offsets when player is facing left

--- core/src/main/java/smashdudes/ecs/systems/RenderDebugSystem.java
+++ core/src/main/java/smashdudes/ecs/systems/RenderDebugSystem.java
@@ -69,12 +69,12 @@ public class RenderDebugSystem extends GameSystem
             sh.setColor(Color.RED);
             for (Rectangle hurtbox : d.hurtboxes)
             {
-                sh.rect(hurtbox.x + p.position.x + hurtbox.width / 2, hurtbox.y + p.position.y - hurtbox.height / 2, -hurtbox.width, hurtbox.height);
+                sh.rect(-hurtbox.x + p.position.x + hurtbox.width / 2, hurtbox.y + p.position.y - hurtbox.height / 2, -hurtbox.width, hurtbox.height);
             }
             sh.setColor(Color.BLUE);
             for (Rectangle hitbox : d.hitboxes)
             {
-                sh.rect(hitbox.x + p.position.x + hitbox.width / 2, hitbox.y + p.position.y - hitbox.height / 2, -hitbox.width, hitbox.height);
+                sh.rect(-hitbox.x + p.position.x + hitbox.width / 2, hitbox.y + p.position.y - hitbox.height / 2, -hitbox.width, hitbox.height);
             }
         }
         else
