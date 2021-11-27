7749b2c5bd0f659954221be3435981eeec808044
Daniel Gold <gold.daniel@outlook.com>
2021-11-26 23:01:05 -0600

    added gradle module for our box tool

---
+++ box-tool/src/main/java/smashdudes/core/boxtool/Lwjgl3Launcher.java
@@ -0,0 +1,30 @@
+package smashdudes.core.boxtool;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import smashdudes.core.Entry;
+
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
+public class Lwjgl3Launcher
+{
+    public static void main(String[] args)
+    {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication()
+    {
+        return new Lwjgl3Application(new Entry(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration()
+    {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("smash-dudes");
+        configuration.setWindowedMode(640, 480);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
