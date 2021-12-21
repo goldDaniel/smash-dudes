b0b2e5540330d5d60862be9982534e0e524146ee
Vessalia <nathanielples@outlook.com>
2021-12-20 21:55:09 -0600

    bug fixes

--- box-tool/src/main/java/smashdudes/core/boxtool/presentation/widgets/CharacterEditorWidget.java
+++ box-tool/src/main/java/smashdudes/core/boxtool/presentation/widgets/CharacterEditorWidget.java
@@ -82,28 +82,34 @@ public class CharacterEditorWidget
         ImGui.sameLine();
         ImFloat jump = new ImFloat();
         jump.set(character.jumpStrength);
+        ImGui.pushID(1 + "");
         if(ImGui.inputFloat("", jump))
         {
             commandList.execute(new jumpEditCommand(character, jump.get()));
         }
+        ImGui.popID();
 
         ImGui.text("Gravity: ");
         ImGui.sameLine();
         ImFloat gravity = new ImFloat();
         gravity.set(character.gravity);
+        ImGui.pushID(2 + "");
         if(ImGui.inputFloat("", gravity))
         {
             commandList.execute(new gravityEditCommand(character, gravity.get()));
         }
+        ImGui.popID();
 
         ImGui.text("Weight: ");
         ImGui.sameLine();
         ImFloat weight = new ImFloat();
         weight.set(character.weight);
+        ImGui.pushID(3 + "");
         if(ImGui.inputFloat("", weight))
         {
             commandList.execute(new weightEditCommand(character, weight.get()));
         }
+        ImGui.popID();
 
         ImGui.separator();
 
