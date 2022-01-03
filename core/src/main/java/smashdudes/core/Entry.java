3d24137729c237b6f45ba71e5cf325b87dfed1a0
Daniel Gold <gold.daniel@outlook.com>
2022-01-02 19:34:37 -0600

    Moved graphics code into its own package

--- core/src/main/java/smashdudes/core/Entry.java
+++ core/src/main/java/smashdudes/core/Entry.java
@@ -1,16 +1,8 @@
 package smashdudes.core;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import smashdudes.ecs.Engine;
-import smashdudes.ecs.Entity;
-import smashdudes.ecs.components.*;
+import smashdudes.graphics.RenderResources;
 import smashdudes.screens.CharacterSelectScreen;
-import smashdudes.screens.GameplayScreen;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
