889080f1c6596c8d9d6c0b8ba717617cf3eb26ff
Daniel Gold <gold.daniel@outlook.com>
2022-02-20 03:20:56 -0600

    Added attack animation

--- core/src/main/java/smashdudes/screens/GameplayScreen.java
+++ core/src/main/java/smashdudes/screens/GameplayScreen.java
@@ -194,7 +194,7 @@ public class GameplayScreen extends GameScreen
             for (DTO.AnimationFrame dtoFrame : anim.frames)
             {
                 AnimationFrame frame =
-                        new AnimationFrame(RenderResources.getTextureDownsampled(dtoFrame.texturePath, 96), dtoFrame.attackboxes, dtoFrame.bodyboxes);
+                        new AnimationFrame(RenderResources.getTextureDownsampled(dtoFrame.texturePath, 64), dtoFrame.attackboxes, dtoFrame.bodyboxes);
                 frames.add(frame);
             }
 
