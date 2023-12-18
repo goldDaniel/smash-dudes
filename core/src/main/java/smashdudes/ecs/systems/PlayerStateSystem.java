e5c69da3bc92102b48bfadf7522f3a557f8291a7
Daniel Gold <contact@golddaniel.ca>
2023-12-18 16:31:36 -0600

    Remove projectiles (#60)

--- core/src/main/java/smashdudes/ecs/systems/PlayerStateSystem.java
+++ core/src/main/java/smashdudes/ecs/systems/PlayerStateSystem.java
@@ -1,11 +1,5 @@
 package smashdudes.ecs.systems;
 
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ArrayMap;
-import smashdudes.core.PlayerHandle;
-import smashdudes.core.Projectile;
 import smashdudes.ecs.Engine;
 import smashdudes.ecs.Entity;
 import smashdudes.ecs.components.*;
@@ -14,10 +8,6 @@ import smashdudes.gameplay.PlayerState;
 
 public class PlayerStateSystem extends GameSystem
 {
-
-    // NOTE (danielg): These are from the old attack system. refactor out
-    private Array<PlayerHandle> hasEntered = new Array<>();
-    
     public PlayerStateSystem(Engine engine)
     {
         super(engine);
@@ -64,40 +54,8 @@ public class PlayerStateSystem extends GameSystem
 
         AnimationComponent anim = entity.getComponent(AnimationComponent.class);
 
-        if(!hasEntered.contains(player.handle, true))
-        {
-            for (Projectile projectile : anim.getCurrentFrame().projectiles)
-            {
-                Entity bullet = engine.createEntity();
-
-                ProjectileComponent proj = new ProjectileComponent();
-                proj.owner = entity;
-                proj.dim = projectile.dim.cpy();
-                proj.damage = projectile.damage;
-                proj.knockback = projectile.knockback;
-                proj.lifeTime = projectile.lifeTime;
-                bullet.addComponent(proj);
-
-                PositionComponent pos = new PositionComponent(new Vector2(projectile.pos.x + entity.getComponent(PositionComponent.class).position.x,
-                        projectile.pos.y + entity.getComponent(PositionComponent.class).position.y));
-                bullet.addComponent(pos);
-
-                VelocityComponent vel = new VelocityComponent();
-                int dir = player.facingLeft ? -1 : 1;
-                vel.velocity.set(dir * projectile.speed.x, projectile.speed.y);
-                bullet.addComponent(vel);
-
-                DrawComponent draw = new DrawComponent();
-                draw.texture = projectile.texture;
-                bullet.addComponent(draw);
-
-                hasEntered.add(player.handle);
-            }
-        }
-
         if(anim.isFinished())
         {
-            hasEntered.removeValue(player.handle, true);
             player.currentState = PlayerState.Ground_Idle;
         }
     }
