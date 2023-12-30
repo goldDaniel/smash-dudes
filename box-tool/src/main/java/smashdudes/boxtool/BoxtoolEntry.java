c4da8454f1667c631650164d3d455b36fc70dee8
Daniel Gold <contact@golddaniel.ca>
2023-12-30 13:38:43 -0600

    rename entrypoint files

--- box-tool/src/main/java/smashdudes/boxtool/BoxtoolEntry.java
+++ box-tool/src/main/java/smashdudes/boxtool/BoxtoolEntry.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 /**
  * Launches the desktop (LWJGL3) application.
  */
-public class Lwjgl3Launcher
+public class BoxtoolEntry
 {
     public static void main(String[] args)
     {
