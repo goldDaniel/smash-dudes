295ebb89482ab33c1eaa535fba02be0013fd688b
Daniel Gold <contact@golddaniel.ca>
2023-12-31 11:24:02 -0600

    move selection type

--- box-tool/src/main/java/smashdudes/core/logic/selectable/SelectableMovable.java
+++ box-tool/src/main/java/smashdudes/core/logic/selectable/SelectableMovable.java
@@ -1,10 +1,14 @@
 package smashdudes.core.logic.selectable;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
+
 
 public class SelectableMovable extends Selectable<Vector2>
 {
+    private boolean selected = false;
     private final float moveSelectionRadius = 0.04f;
 
     public SelectableMovable(OnApply apply)
@@ -15,18 +19,48 @@ public class SelectableMovable extends Selectable<Vector2>
     @Override
     public boolean select(Vector2 mouseWorldPos)
     {
-        return false;
+        selected = false;
+        if(getGrabCircle(original).contains(mouseWorldPos))
+        {
+            selected = true;
+            clone.set(original);
+        }
+
+        return selected;
     }
 
     @Override
     public void drag(Vector2 mouseWorldPos, Vector2 mouseDelta)
     {
-
+        clone.x = mouseWorldPos.x;
+        clone.y = mouseWorldPos.y;
     }
 
     @Override
     public void draw(Vector2 mouseWorldPos, ShapeRenderer sh)
     {
+        if (sh.isDrawing())
+        {
+            sh.end();
+        }
 
+        Vector2 p = original;
+        Color color = Color.LIGHT_GRAY;
+
+        if(selected)
+        {
+            p = clone;
+            color = Color.WHITE;
+        }
+
+        sh.setColor(color);
+        sh.begin(ShapeRenderer.ShapeType.Filled);
+        sh.circle(p.x, p.y, moveSelectionRadius, 16);
+        sh.end();
+    }
+
+    private Circle getGrabCircle(Vector2 p)
+    {
+        return new Circle(p.x, p.y, moveSelectionRadius);
     }
 }
